32eb6fe1cc005c0a290a295063f9ce8341e1e3e3
SHUNMUGARAJ25 <151824878+SHUNMUGARAJ25@users.noreply.github.com>
2025-04-02 12:20:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let a = [10,20];
+let b = a;
+b.pop()
+console.log(a)
+
+// OBJECT
+const obj = {
+    name: "Some name",
+    type: "Good",
+    fav:["Orange","Apple"],
+};
+const test ="type"
+console.log(obj.fav);
